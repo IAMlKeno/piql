12f9e8fa6152e317c1de3ff87351f3221640bdb3
Elkeno Jones <elkeno.jones@binarystar.ca>
2024-05-05 23:46:17 -0300

    add some notifying comments

--- python/drug_inventory/inventory.py
+++ python/drug_inventory/inventory.py
@@ -1,8 +1,16 @@
+# Importing using libraries into the context of our script
+# Think of this as including a file (instruction) that exists
+# somewhere else.
 from flask import Flask, jsonify
 import subprocess
 
 app = Flask(__name__)
 
+# As we are making web based api, apply an http decorator.
+# This decorator, provided from the flask library, handles
+# make our endpoint access through http tunnels.
+# Following the decorator is our standard method definiton
+# and body.
 @app.route('/getAll', method=['GET'])
 def get_all():
   return jsonify([
@@ -17,3 +25,8 @@ def get_all():
       'i_quantity': 100
     }
   ])
+
+# This starts the loop that is our application - keeping it alive
+# and ready for interaction from the User Interface
+if __name__ == '__main__':
+  app.run(debug=True, port=4201)

8e4122188dcded1e2c74a6fde85fdfe60c81912d
Elkeno Jones <elkeno.jones@binarystar.ca>
2024-06-01 10:09:11 -0300

    feat: update database to be more complex with FKs and more helpful demo data

--- data/database/scripts/creation.sql
+++ data/database/scripts/creation.sql
@@ -1,17 +1,51 @@
-CREATE DATABASE drug_inventory;
+CREATE DATABASE IF NOT EXISTS drug_inventory;
 USE drug_inventory;
 
-CREATE TABLE User (
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users (
   id int NOT NULL AUTO_INCREMENT,
   s_username varchar(255) NOT NULL UNIQUE,
   s_password varchar(255), -- we will eventually turn this into a salted password
   PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS Brands;
+CREATE TABLE Brands (
+  id int NOT NULL AUTO_INCREMENT,
+  s_name varchar(255) NOT NULL,
+  s_location varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS Drugs;
 CREATE TABLE Drugs (
   id int NOT NULL AUTO_INCREMENT,
-  s_brand varchar(255) NOT NULL,
   s_name varchar(255) NOT NULL,
   i_quantity int DEFAULT 0,
+  t_lastupdated DATETIME NULL,
+  i_brandId int,
+  PRIMARY KEY (id),
+  FOREIGN KEY (i_brandId) REFERENCES Brands(id)
+);
+
+DROP TABLE IF EXISTS Actions;
+CREATE TABLE Actions (
+  id int NOT NULL AUTO_INCREMENT,
+  s_description varchar(255) NOT NULL,
+  c_operation char NOT NULL,
   PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS Audits;
+CREATE TABLE Audits (
+  id int NOT NULL AUTO_INCREMENT,
+  i_owner int NOT NULL,
+  i_actionId int NOT NULL,
+  i_value int NOT NULL,
+  i_drugId int NOT NULL,
+  t_timestamp DATETIME NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (i_actionId) REFERENCES Actions(id),
+  FOREIGN KEY (i_owner) REFERENCES Users(id),
+  FOREIGN KEY (i_drugId) REFERENCES Drugs(id)
 );
--- data/database/scripts/data.sql
+++ data/database/scripts/data.sql
@@ -1,17 +1,34 @@
 -- Inject test data
 
-USE drug_inventory;
+/* USE drug_inventory; */
 
-INSERT INTO User (s_username, s_password)
+INSERT INTO Users (id, s_username, s_password)
 VALUES
-  ("User1", "password"),
-  ("User2", "password"),
-  ("User3", "password"),
-  ("User4", "password");
+  (2, "User1", "password"),
+  (3, "User2", "password"),
+  (4, "User3", "password"),
+  (5, "User4", "password");
 
-INSERT INTO Drugs (s_brand, s_name, i_quantity)
+INSERT INTO Brands (id, s_name, s_location) VALUES
+  (1, "Drugs-r-us", "UK"),
+  (2, "Sniff-it", "US");
+
+INSERT INTO Drugs (id, i_brandId, s_name, i_quantity, t_lastupdated)
 VALUES
-  ("Drugs-r-us", "Crap Pills", 500),
-  ("Drugs-r-us", "Super Pills", 100),
-  ("Sniff-it", "The Best Kind", 10),
-  ("Sniff-it", "IDK-38", 5);
+  (1, 1, "Crap Pills", 500, "2024-01-01 00:00:00"),
+  (2, 1, "Super Pills", 100, "2024-01-01 00:00:00"),
+  (3, 2, "The Best Kind", 10, "2024-01-01 00:00:00"),
+  (4, 2, "IDK-38", 5, "2024-01-05 00:00:00");
+
+INSERT INTO Actions (id, s_description, c_operation)
+VALUES
+  (1, "Adding quantity", '+'),
+  (2, "Subtracting quantity", '-');
+
+INSERT INTO Audits (i_owner, i_actionId, i_value, i_drugId, t_timestamp)
+VALUES
+  (2, 1, 500, 1, "2024-01-01 00:00:00"),
+  (3, 1, 100, 2, "2024-01-01 00:00:00"),
+  (4, 1, 10, 3, "2024-01-01 00:00:00"),
+  (5, 1, 20, 4, "2024-01-01 00:00:00"),
+  (2, 2, 15, 4, "2024-01-05 00:00:00");
